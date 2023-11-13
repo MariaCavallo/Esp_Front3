bd0bd555765d2e97ca5a25258539a0a5316a5c54
Maria Cavallo <mariacavallom@gmail.com>
2023-11-13 12:14:01 -0300

    add table of users to newsletter

--- newsletter/src/pages/index.tsx
+++ newsletter/src/pages/index.tsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
-import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormLabel, Grid, InputLabel, ListItem, ListItemButton, ListItemText, MenuItem, Modal, Paper, Radio, RadioGroup, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
+import { rows, columns } from "./data";
+import { DataGrid } from "@mui/x-data-grid";
+import Link from "next/link";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  height: 700,
+  bgcolor: "background.paper",
+  border: "2px solid #131313",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Home() {
 
   const [stack, setStack] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   const handleChange = (event: SelectChangeEvent) => {
     setStack(event.target.value as string)
@@ -16,7 +41,7 @@ export default function Home() {
         sx={{ p: "32px", display: "flex", flexDirection: "column", gap: 3 }}
       >
         <Typography sx={{ fontWeight: 500, fontSize: 24 }}>
-          Suscríbase a nuestro Newsletter! 📰
+          Suscríbase a nuestro Newsletter!📰
         </Typography>
 
         <Grid item xs={12}>
@@ -46,9 +71,9 @@ export default function Home() {
             label="Stack"
             onChange={handleChange}
           >
-            <MenuItem value={"FE"}>Front-end 💻</MenuItem>
-            <MenuItem value={"BE"}>Back-end 👩‍💻</MenuItem>
-            <MenuItem value={"FS"}>Full-stack 📚</MenuItem>
+            <MenuItem value={"FE"}>Front-end💻</MenuItem>
+            <MenuItem value={"BE"}>Back-end👩‍💻</MenuItem>
+            <MenuItem value={"FS"}>Full-stack📚</MenuItem>
           </Select>
         </FormControl>
 
@@ -66,13 +91,43 @@ export default function Home() {
         </FormControl>
 
         <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", alignContent: "center" }}>
-        <Typography sx={{ fontSize: 16 }}>¿Recibir notificaciones? 🔔</Typography>
+        <Typography sx={{ fontSize: 16 }}>Deseo recibir notificaciones🔔</Typography>
           <Checkbox />
         </Box>
 
         <Button variant="contained" sx={{ width: 1 }} onClick={() => alert("Se ha suscripto con éxito, Bienvenid@! 🙌")}>
-          Suscribirse 📝
+          Suscribirse📝
         </Button>
+
+        <Box sx={{ textAlign: "center", mt: 4 }}>
+          <Button onClick={handleOpen} sx={{ fontSize: 16 }}>
+            Lista de suscriptores📋
+          </Button>
+        </Box>
+
+        <Modal
+          open={open}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Box sx={{ width: "100%", height: 500, bgcolor: "background.paper" }}>
+              <Typography sx={{ fontSize: 24, textAlign: "center", mb: 4, fontWeight: 500, paddingTop: "19px", color: "#1976D2"}}>
+                Personas que se suscribieron a nuestro Newsletter
+              </Typography>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSizeOptions={[5]}
+                disableRowSelectionOnClick
+              />
+              <Typography component={"a"} onClick={handleClose} sx={{cursor: "pointer", color: "#1976D2"}}>
+                Cerrar
+              </Typography>
+            </Box>
+          </Box>
+        </Modal>
+        
       </Paper>
     </Box>
   )
